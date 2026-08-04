f588ffc67493200be2d5a6d0d4fd6b2e0616d2db
Harley Kradovill <harleykradovill@gmail.com>
2026-08-04 12:43:02 -0600

    add styles for config screen, add keyboard navigation

--- src/jellybench/screens/configuration.py
+++ src/jellybench/screens/configuration.py
@@ -1,19 +1,36 @@
+from ..ascii import LOGO
 from textual.app import ComposeResult
+from textual.containers import Container
 from textual.screen import Screen
-from textual.widgets import Input, Button, Label, Header, Footer
+from textual.widgets import Input, Button, Label, Static
 
 
 class ConfigurationScreen(Screen):
     def compose(self) -> ComposeResult:
-        yield Label("Server URL")
-        yield Input(self.app.config.server.url, id="url")
-        yield Label("API Key")
-        yield Input(self.app.config.server.api_key, id="api_key", password=True)
-        yield Button("Save", id="save")
+        yield Container(
+            Label("Server URL"),
+            Input(self.app.config.server.url, id="url"),
+            Label("API Key"),
+            Input(self.app.config.server.api_key, id="api_key", password=True),
+            Button("Save", id="save"),
+            id="config-container",
+        )
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "save":
-            self.app.config.server.url = self.query_one("#url", Input).value
-            self.app.config.server.api_key = self.query_one("#api_key", Input).value
-            self.app.config.save()
-            self.app.pop_screen()
+            self.save()
+
+    def key_enter(self) -> None:
+        self.save()
+
+    def key_down(self) -> None:
+        self.focus_next()
+
+    def key_up(self) -> None:
+        self.focus_previous()
+
+    def save(self) -> None:
+        self.app.config.server.url = self.query_one("#url", Input).value
+        self.app.config.server.api_key = self.query_one("#api_key", Input).value
+        self.app.config.save()
+        self.app.pop_screen()
